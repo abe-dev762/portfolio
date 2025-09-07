00dda1ce28e3c692b99735451f544f7b37c62ce1
Ahmad Zaenal Abidin <ahmadabidin762.dev@gmail.com>
2025-09-07 12:52:38 +0700

    about page added

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -41,4 +41,29 @@ export const projects: Project[] = [
         demoLink: 'https://finsight-app-dun.vercel.app/',
         image: '/project/finsight.png',
       },
+      {
+        title: 'Social Media App',
+        description: 'A full-stack social media platform built with Next.js, TypeScript, and Tailwind CSS.',
+        technologies: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Potsgresql', 'Prisma ORM'],
+        githubLink: 'https://github.com/abe-dev762/luno-app',
+        demoLink: 'https://luno-app-zeta.vercel.app/',
+        image: '/project/luno.png',
+      },
+      {
+        title: 'Voting/Poll App',
+        description: 'A full-stack voting app built with Spring Boot, Java, Angular, MySQL and Tailwind CSS.',
+        technologies: ['Spring Boot', 'Java', 'Angular' ,'Tailwind CSS', 'MySQL'],
+        githubLink: 'https://github.com/abe-dev762/voting-poll-App',
+        demoLink: 'https://hub.docker.com/r/abe762/voting-app',
+        image: '/project/voting-app.png',
+      },
+      {
+        title: 'Real-time Chat App',
+        description: 'A full-stack real-time chat app built with Spring Boot, Java, Thymeleaf, and WebSocket.',
+        technologies: ['Spring Boot', 'Java', 'Thymeleaf' ,'WebSocker'],
+        githubLink: 'https://github.com/abe-dev762/chat-app',
+        demoLink: 'https://hub.docker.com/r/abe762/chatapp',
+        image: '/project/chat-app.png',
+      },
+
 ]
